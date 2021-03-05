d9ccaa80dd4035363678ee90673fa27579936440
Атрощенко Константин Анатольевич <asu8@vts.vitebsk.energo.net>
2021-03-05 15:03:09 +0300

    CreateHook 001

--- src/App.js
+++ src/App.js
@@ -1,17 +1,57 @@
 //App.js
-import React from 'react';
-import Main from './Main';
-import Alert from './alert/Alert';
-import { AlertProvider } from './alert/AlertContext';
+import React, { useEffect, useState } from 'react';
+
+function useLogger(value, lastName) {
+  useEffect(() => {
+    console.log('Value changed', value, lastName);
+  }, [value, lastName]);
+}
+
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const clear = () => {
+    setValue('');
+  };
+
+  return {
+    bind: {
+      value,
+      onChange,
+    },
+    value,
+    clear,
+  };
+}
 
 function App() {
+  const input = useInput('');
+  const lastName = useInput('');
+
+  useLogger(input.value);
+
   return (
-    <AlertProvider>
-      <div className={'container pt-3'}>
-        <Alert />
-        <Main toggle={() => {}} />
-      </div>
-    </AlertProvider>
+    <div className={'container pt-3'}>
+      <input type="text" {...input.bind} />
+      <button className="btn btn-warning" onClick={() => input.clear()}>
+        Очистить
+      </button>
+      <br />
+      <br />
+      <br />
+      <input type="text" {...lastName.bind} />
+
+      <button className="btn btn-warning" onClick={() => lastName.clear()}>
+        Очистить
+      </button>
+      <hr />
+      <h1>{input.value}</h1>
+      <h1>{lastName.value}</h1>
+    </div>
   );
 }
 
