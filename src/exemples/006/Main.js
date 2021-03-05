f0ddc9368961f193d2e8d3404e53c5e6b6d05ded
Атрощенко Константин Анатольевич <asu8@vts.vitebsk.energo.net>
2021-03-05 10:42:45 +0300

    useReduser 001

--- src/exemples/006/Main.js
+++ src/exemples/006/Main.js
@@ -3,14 +3,11 @@ import React from 'react';
 import { useAlert } from './alert/AlertContext';
 
 export default function Main() {
-  const { show } = useAlert();
+  const { toggle } = useAlert();
   return (
     <>
       <h1>Привет в примере с Context</h1>
-      <button
-        onClick={() => show('Этот текст из Main.js')}
-        className="btn btn-success"
-      >
+      <button onClick={toggle} className="btn btn-success">
         Показать alert{' '}
       </button>
     </>
